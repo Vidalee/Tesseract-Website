390fdbe6dcecc4dc5dc710888520b314b42e342f
Vidalee <thomas.sirvent@free.fr>
2019-01-18 00:23:08 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+app.use(express.static('res'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/res/cat.gif')
+})
+
+app.listen(42042, function () {
+  console.log('Tesseract website listening on port 42042!')
+})
